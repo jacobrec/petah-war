5a8bc915bd085da6048c173cfbf06d6cfdf41e7d
Jacob Reckhard <jacobreckhard@gmail.com>
2019-11-14 15:49:03 -0700

    unit

--- world/terrain.rkt
+++ world/terrain.rkt
@@ -4,7 +4,16 @@
 (provide (all-defined-out))
 
 (define (terrain-movement-usage unit tile)
-  (cond [(= tile TILE_MOUNTAIN) 6]
-        [(= tile TILE_FOREST) 4]
-        [(= tile TILE_ROAD) 1]
-        [else 2]))
+  (define NO 1000000)
+  (if (or (= unit UNIT_PLANE)
+          (= unit UNIT_BOMBER)
+          (= unit UNIT_HELICOPTER))
+    1
+    (if (or (= unit UNIT_FERRY)
+            (= unit UNIT_BATTLESHIP)
+            (= unit UNIT_FERRY))
+        (if (= TILE_WATER tile) 1 NO)
+        (cond [(= tile TILE_MOUNTAIN) 6]
+              [(= tile TILE_FOREST) 4]
+              [(= tile TILE_ROAD) 1]
+              [else 2]))))
